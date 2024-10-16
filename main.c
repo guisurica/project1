7d6a9f0d181244d2d878d83f2098f8d7aae4c8fb
guisurica <eosurica@gmail.com>
2024-10-16 17:11:47 -0300

    refact: adding the window. Packages mesa, glfw included

--- main.c
+++ main.c
@@ -1,24 +1,23 @@
+#include <GL/gl.h>
 #include <GLFW/glfw3.h>
-#include <stdio.h>
-
-void error_callback(int error, const char* description) {
-  fprintf(stderr, "Error: %s\n", description);
-}
 
 int main(int argc, char** argv) {
-  
-  glfwSetErrorCallback(error_callback);
   glfwInit();
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-  GLFWwindow* window = glfwCreateWindow(640, 480, "My window", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(640, 480, "My window", NULL, NULL); 
+  
   glfwMakeContextCurrent(window);
-  
+
   while(!glfwWindowShouldClose(window)) {
-    
+    glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
+    glfwPollEvents();
+    glfwSwapBuffers(window);
   }
-  
+
+
   glfwDestroyWindow(window);
   glfwTerminate();
+
+  return 0;
 }
